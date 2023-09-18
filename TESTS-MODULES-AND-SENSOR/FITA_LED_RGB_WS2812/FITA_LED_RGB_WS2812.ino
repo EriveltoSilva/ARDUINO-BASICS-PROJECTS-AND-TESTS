dbcf04fc8da67c6294ab368cecd38c4945fc7e89
otlevire <eriveltoclenio@gmail.com>
2023-09-18 23:13:32 +0100

    feat: mode auto and mode, mode selected
    -added mode automatic
    -added start color-white
    -added selected number leds on

--- TESTS-MODULES-AND-SENSOR/FITA_LED_RGB_WS2812/FITA_LED_RGB_WS2812.ino
+++ TESTS-MODULES-AND-SENSOR/FITA_LED_RGB_WS2812/FITA_LED_RGB_WS2812.ino
@@ -1,8 +1,4 @@
-
 #include <FastLED.h>
-
-// How many leds in your strip?
-#define NUM_LEDS 5
 
 // For led chips like WS2812, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -10,12 +6,18 @@
 // Clock pin only needed for SPI based chipsets when not using hardware SPI
 #define DATA_PIN 7
 #define CLOCK_PIN 13
-#define BOTAO 8
+#define BOTAO_AUTO 8
+#define BOTAO_TROCAR 9
+#define BOTAO_NUM_LEDS 10
 
+#define NUM_LEDS 47 
 
 byte contadorCor = 0;
+unsigned int inicio = 0;
+unsigned int fim = 47;
 unsigned long int tempoDelay=0;
-// Define the array of leds
+
+
 CRGB cor= CRGB::White;
 CRGB leds[NUM_LEDS];
 CRGB cores[]={
@@ -27,35 +29,48 @@ CRGB cores[]={
 
 
 void setup() { 
-  pinMode(BOTAO, INPUT_PULLUP);
+  pinMode(BOTAO_AUTO, INPUT_PULLUP);
+  pinMode(BOTAO_TROCAR, INPUT_PULLUP);
+  pinMode(BOTAO_NUM_LEDS, INPUT_PULLUP);
+  delay(1000);
+  if(digitalRead(BOTAO_NUM_LEDS))
+  {
+    inicio = 18;
+    fim=40;
+  }
+  else{
+    inicio = 9;
+    fim=47;
+  }
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
 
-  for(byte i=0; i<NUM_LEDS; i++)
+  for(byte i=inicio; i<fim; i++)
     leds[i] = cor;
   FastLED.show();  
 }
 
 void loop() 
 { 
-  
-  if((millis()-tempoDelay)>(1000*5))
+  if(!digitalRead(BOTAO_AUTO)) // Trocar Automaticamente
   {
-    tempoDelay= millis();
-    trocarCor();
+    if((millis()-tempoDelay)>(1000*5))
+    {
+      tempoDelay= millis();
+      trocarCor();
+    }
   }
 
-  if(!digitalRead(BOTAO))
+  if(!digitalRead(BOTAO_TROCAR))// Trocar Manualmente
   {
-    while(!digitalRead(BOTAO));
+    while(!digitalRead(BOTAO_TROCAR));
     trocarCor();
     delay(1500);
   }
 
   // Turn the LED on, then pause
-  for(byte i=0; i<NUM_LEDS; i++)
+  for(byte i=inicio; i<fim; i++)
     leds[i] = cor;
   FastLED.show();
-  
   delay(500);
 }
 
